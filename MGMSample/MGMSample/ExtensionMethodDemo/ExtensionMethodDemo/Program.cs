494e1555fff8082799262d75ca27e16f7144a5a8
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-10-10 09:17:57 +0530

    extension method practice

---
+++ MGMSample/MGMSample/ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodDemo
+{
+    class A
+    {
+        public void Show()
+        {
+            Console.WriteLine("I am show");
+        }
+    }
+    static class NewClass
+    {
+        public static void NewMethod(this A obj)
+        {
+            Console.WriteLine("I am new method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            a1.Show();
+            a1.NewMethod();
+        }
+    }
+}

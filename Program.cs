998bb12a522dec252a4c18701b33aa6b2df9bc46
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-10-27 15:31:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MgmDIDemo;
+using Microsoft.Extensions.DependencyInjection;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup DI
+        var serviceProvider = new ServiceCollection()
+            .AddTransient<IMilk, CowMilk>()
+            .AddTransient<ISugar, WhiteSugar>()
+            .AddTransient<CoffeeMachine>()
+            .BuildServiceProvider();
+        
+        // Resolve CoffeeMachine
+        var coffeeMachine = serviceProvider.GetService<CoffeeMachine>();
+        // Make coffee
+        if (coffeeMachine != null)
+        {
+            coffeeMachine.MakeCoffee();
+        }
+        else
+        {
+            Console.WriteLine("Failed to resolve CoffeeMachine from DI container.");
+        }
+    }
+}
---
+++ WhiteSugar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MgmDIDemo
+{
+    internal class WhiteSugar: ISugar
+    {
+        public string GetSugar()
+        {
+            return "White Sugar";
+        }
+    }
+}

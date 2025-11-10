37b1020ae90776aba71592e1f5294a6672612849
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-11-10 18:35:22 +0530

    Add files via upload

---
+++ Practice Code/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCourse()
+        {
+                       return View();   
+        }
+    }
+}
---
+++ Practice Code/MyGoMindsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCapp.Controllers
+{
+    public class MyGoMindsController : Controller
+    {
+        // GET: MyGoMinds
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}

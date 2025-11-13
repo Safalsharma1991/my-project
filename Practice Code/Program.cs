11cbaced32b15f50b65ad2c9845fa2c4bf92c4ed
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-11-13 15:39:20 +0530

    Add files via upload

--- Practice Code/Program.cs
+++ Practice Code/Program.cs
@@ -1,18 +1,24 @@
-﻿using ConsoleApp1;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace MGMGeneric
 {
-    public static void Main(string[] args)
+    internal class Program
     {
-        var services = new ServiceCollection();
-        services.AddTransient<IMilk, CowMilk>();
-        services.AddTransient<ISugar, BrownSugar>();
-        services.AddTransient<CoffeeMachine>();
-        var serviceProvider = services.BuildServiceProvider();
-        var coffeeMachine = serviceProvider.GetRequiredService<CoffeeMachine>();
-        coffeeMachine.MakeCoffee();
-
-
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(10);
+            ar.Add("MyGoMinds");
+            ar.Add(2.5); ar.Add(true);
+            foreach (var i in ar)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
